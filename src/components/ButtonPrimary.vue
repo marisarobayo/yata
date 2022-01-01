48e0fb7572c9c80e3c3208039a5a5e725f7345dd
Marisa Robayo <marisarobayo@gmail.com>
2022-01-01 12:15:52 +0100

    Sketches the home page.

---
+++ src/components/ButtonPrimary.vue
@@ -0,0 +1,37 @@
+
+<template>
+  <button
+    type="button"
+    :class="$style.button"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent  } from "vue";
+
+export default defineComponent({
+  name: 'ButtonPrimary',
+})
+</script>
+
+<style module lang="scss">
+  .button {
+    display: inline-flex;
+    align-items: center;
+    padding: 0.75rem 1.25rem;
+    font-size: small;
+    font-weight: 600;
+    border-radius: 9999px;
+    box-shadow: 0 1px 2px 0 rgb(0 0 0 / 0.05);
+    cursor: pointer;
+    background-color: #4461ee;
+    color: white;
+    border: transparent;
+
+    &:hover {
+      background-color: #0f28a4;
+    }
+  }
+</style>
---
+++ src/views/home/Home.vue
@@ -0,0 +1,74 @@
+
+<template>
+  <nav :class="$style.bar">
+    <div :class="$style.logodiv">
+      <span :class="$style.logo">YATA</span>
+    </div>
+    <div :class="$style.logindiv">
+      <ButtonPrimary>Iniciar sesión</ButtonPrimary>
+    </div>
+  </nav>
+  <div :class="$style.pitch">
+    <div>
+      <p :class="$style.explanationTitle">
+        Recompénsate por tu esfuerzo
+      </p>
+      <p :class="$style.explanationDesc">
+        Con YATA, puedes fijar tareas que hacer a diferentes intervalos del día. YATA te recordará.
+        Al completarlas, según su dificultad, consiguirás monedas que podrás canjear por recompensas puestas por ti.
+      </p>
+    </div>
+    <div>
+      <img src="https://place-hold.it/300x500"/>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import ButtonPrimary from '@/components/ButtonPrimary.vue';
+
+export default defineComponent({
+  name: 'Home',
+  components: {
+    ButtonPrimary
+},
+});
+</script>
+
+<style module>
+  .bar {
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
+    max-width: 1400px;
+    margin-left: auto;
+    margin-right: auto;
+  }
+  .logodiv {
+    padding: 0.5rem;
+  }
+  .logindiv {
+    padding: 0.5rem;
+  }
+  .logo {
+    font-size: x-large;
+    font-weight: 600;
+  }
+  .pitch {
+    max-width: 1400px;
+    margin-left: auto;
+    margin-right: auto;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    margin: 4rem auto 4rem auto;
+  }
+  .explanationTitle {
+    font-size: xx-large;
+    font-weight: 600;
+  }
+  .explanationDesc {
+    font-size: large;
+  }
+</style>
