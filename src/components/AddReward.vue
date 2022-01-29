c5f41ca66b886ebb492892154dca5a7c321e5b54
Marisa Robayo <marisarobayo@gmail.com>
2022-01-29 12:15:32 +0100

    Designs the reward form.

---
+++ src/components/AddReward.vue
@@ -0,0 +1,51 @@
+
+<template>
+  <span class="inline-flex justify-center items-center py-1 gap-x-2">
+    <Input
+      name="reward"
+      placeholder="Añade una recompensa"
+      v-model="rewardName"
+    />
+    <Input
+      name="rewardCost"
+      placeholder="Coste"
+      type="number"
+      v-model="rewardCost"
+      class="w-24"
+    />
+    <ButtonPrimary>
+      <font-awesome-icon icon="plus" @click="submitTask" />
+    </ButtonPrimary>
+  </span>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+import ButtonPrimary from "./ButtonPrimary.vue";
+import Input from "./Input.vue"
+
+export default defineComponent({
+    name: "AddTask",
+    components: {
+      ButtonPrimary,
+      Input,
+    },
+    setup (_props, context) {
+      const rewardName = ref('')
+      const rewardCost = ref('')
+
+      const submitTask = () => {
+        if (rewardName.value && rewardCost.value) {
+          context.emit('addReward', rewardName.value, rewardCost.value)
+        }
+      }
+
+      return {
+        rewardName,
+        rewardCost,
+        submitTask,
+      }
+    },
+    emits: ['addReward'],
+})
+</script>
--- src/components/Input.vue
+++ src/components/Input.vue
@@ -1,10 +1,10 @@
 
 <template>
   <input
-    type="text"
+    :type="type"
     :name="name"
     :id="name"
-    class="shadow-sm focus:ring-teal-500 focus:border-teal-500 block w-full sm:text-sm border-gray-300 rounded-md"
+    class="shadow-sm focus:ring-teal-500 focus:border-teal-500 block sm:text-sm border-gray-300 rounded-md"
     :placeholder="placeholder"
     :value="modelValue"
     @input="$emit('update:modelValue', ($event.target as HTMLInputElement).value)"
@@ -12,7 +12,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, PropType } from "vue";
 
 export default defineComponent({
     name: "Input",
@@ -20,6 +20,10 @@ export default defineComponent({
       name: String,
       placeholder: String,
       modelValue: String,
+      type: {
+        type: String as PropType<("text" | "number")>,
+        default: "text",
+      },
     },
     emits: ['update:modelValue'],
 })
