f6c144ef0aac818cec44e151402139197f96391d
Marisa Robayo <marisarobayo@gmail.com>
2022-02-03 20:13:03 +0100

    Logs in an user.

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,49 @@
+import router from '@/router'
+import { auth } from '@/services/firebase'
+import { signInWithEmailAndPassword } from 'firebase/auth'
 import { createStore } from 'vuex'
 
+const userItem = localStorage?.getItem('user')
+const initialState = userItem ? {
+  status: { loggedIn: true },
+  user: JSON.parse(userItem),
+}
+:
+{
+  status: { loggedIn: false },
+  user: null
+}
+
+console.log(initialState)
+
 export default createStore({
-  state: {
-  },
+  state: initialState,
   mutations: {
+    loginSuccess(state, user) {
+      state.status.loggedIn = true
+      state.user = user
+    },
+    loginFailure(state) {
+      state.status.loggedIn = false;
+      state.user = null;
+    },
+    logout(state) {
+      state.status.loggedIn = false;
+      state.user = null;
+    },
   },
   actions: {
+    login({ commit }, user) {
+      signInWithEmailAndPassword(auth, user.email, user.password)
+        .then((cred) => {
+          commit('loginSuccess', cred.user)
+          localStorage.setItem('user', JSON.stringify(cred.user));
+          router.push('/dashboard')
+        })
+    },
+    logout({ commit }) {
+      //logout here
+    },
   },
   modules: {
   }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,34 +1,78 @@
 
 <template>
-  <div class="max-w-7xl mx-auto flex justify-around items-center my-16">
-    <div>
-      <p class="text-3xl font-medium">
-        Recompénsate por tu esfuerzo
-      </p>
-      <p class="text-2xl">
-        Con YATA, puedes fijar tareas que hacer a diferentes intervalos del día. YATA te recordará.
-        Al completarlas, según su dificultad, consiguirás monedas que podrás canjear por recompensas puestas por ti.
-      </p>
+  <div class="max-w-7xl flex flex-col mx-auto my-16">
+    <div class="sm:mx-auto sm:w-full sm:max-w-md">
+      <h2 class="mt-6 text-center text-4xl font-medium text-gray-900">
+        Inicia sesión
+      </h2>
     </div>
-    <div>
-      <img src="https://place-hold.it/300x500"/>
+
+    <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+      <div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+        <form class="space-y-6" action="#" method="POST">
+          <Input
+            name="email"
+            placeholder="Email"
+            v-model="email"
+            label="Email"
+          />
+          <Input
+            name="password"
+            v-model="password"
+            label="Password"
+            type="password"
+          />
+          <div>
+            <button @click.prevent="submit" class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+              Iniciar sesión
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+    <div class="flex justify-around items-center">
+      <div>
+        <p class="text-3xl font-medium">
+          Recompénsate por tu esfuerzo
+        </p>
+        <p class="text-xl mt-2">
+          Con YATA, puedes fijar tareas que hacer a diferentes intervalos del día. YATA te recordará.
+          Al completarlas, según su dificultad, consiguirás monedas que podrás canjear por recompensas puestas por ti.
+        </p>
+      </div>
+      <div>
+        <img src="https://place-hold.it/300x500"/>
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { User } from '@/utils/models';
-import { defineComponent, PropType } from 'vue'
-import router from '@/router';
+import { defineComponent, ref } from 'vue'
+import { useStore } from 'vuex';
+
+import Input from '@/components/Input.vue';
 
 export default defineComponent({
   name: 'Home',
-  props: {
-    user: Object as PropType<User>
-  },
-  setup: (props) => {
-    if(props.user) {
-      router.push('/dashboard')
+  components: { Input },
+  setup: () => {
+    let email = ref("")
+    let password = ref("")
+
+    const store = useStore()
+
+    const submit = () => {
+      store.dispatch('login', {
+        email: email.value,
+        password: password.value,
+      })
+    }
+
+    return {
+      email,
+      password,
+      submit,
     }
   }
 });
