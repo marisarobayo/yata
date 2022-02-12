863c5e27900eab49e267fd23b5b22c2758fb1a35
Marisa Robayo <marisarobayo@gmail.com>
2022-02-12 15:51:39 +0100

    Removes typescript errors on services.

--- src/services/tasks.ts
+++ src/services/tasks.ts
@@ -1,5 +1,5 @@
 
-import { Task } from "@/utils/models"
+import { DaysOfWeek, Task } from "@/utils/models"
 import { collection } from "firebase/firestore"
 import { db } from "./firebase"
 
@@ -7,19 +7,56 @@ const tasksService = collection(db, "tasks")
 
 export default tasksService
 
-export function firestoreTaskConverter(task: any): Task {
+export interface FirestoreTimestamp {
+  seconds: number,
+  toDate: () => Date,
+}
+
+export interface TaskFrequencyWeekly {
+  time: FirestoreTimestamp,
+  daysOfWeek: DaysOfWeek[],
+}
+
+interface FirestoreTask {
+  id: string,
+  title?: string,
+  description?: string,
+  difficulty?: 1 | 2 | 3 | 4,
+  frequency: FirestoreTimestamp | TaskFrequencyWeekly,
+  user: string,
+}
+
+interface FirestoreTaskGetter {
+  id: string,
+  data: () => FirestoreTask,
+}
+
+function isFirestoreTimestamp(obj: FirestoreTimestamp | TaskFrequencyWeekly): obj is FirestoreTimestamp {
+  return 'seconds' in obj
+}
+
+export function firestoreTaskConverter(task: FirestoreTaskGetter): Task {
   const newTask = task.data()
 
-  if (newTask.frequency.seconds) {
-    newTask.frequency = newTask.frequency.toDate()
-  } else if (newTask.frequency.time) {
-    newTask.frequency.time = newTask.frequency.time.toDate()
+  let result: Task
+  if (isFirestoreTimestamp(newTask.frequency)) {
+    result = {
+      ...newTask,
+      id: task.id,
+      frequency: newTask.frequency.toDate(),
+    }
+  } else {
+    result = {
+      ...newTask,
+      id: task.id,
+      frequency: {
+        ...newTask.frequency,
+        time: newTask.frequency.time.toDate(),
+      },
+    }
   }
 
-  return {
-    ...newTask,
-    id: task.id
-  }
+  return result
 }
 
 //TODO service functions go here
