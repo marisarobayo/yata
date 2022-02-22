9e2bd3eb4bf90e9576a54f62257f1dcf400e0d7c
Marisa Robayo <marisarobayo@gmail.com>
2022-02-22 16:25:38 +0100

    Adds opacity to the modal.

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -2,7 +2,7 @@
 <template>
   <div v-if="open">
     <Teleport to="body">
-      <div class="fixed top-0 bottom-0 left-0 right-0 bg-zinc-500 z-50" @click="$emit('close')">
+      <div class="fixed top-0 bottom-0 left-0 right-0 bg-zinc-500 bg-opacity-50 z-50" @click="$emit('close')">
         <div class="m-auto p-12 max-w-5xl">
           <div class="bg-white rounded-xl" @click.prevent.stop>
             <div class="pt-4 pb-4 px-12 border-b border-zinc-300 shadow-sm inline-flex w-full justify-between items-center text-2xl">
