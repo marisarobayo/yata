9018cc7a85ae5cbaf1248d38963e6028df74b63b
Marisa Robayo <marisarobayo@gmail.com>
2022-02-23 15:23:50 +0100

    Designs a Textarea component.

---
+++ src/components/Textarea.vue
@@ -0,0 +1,31 @@
+
+<template>
+  <div>
+    <label :for="name" class="block text-sm font-medium text-gray-700">{{label}}</label>
+    <div class="mt-1">
+      <textarea
+        rows="4"
+        :name="name"
+        :id="name"
+        class="shadow-sm focus:ring-teal-500 focus:border-teal-500 block w-full sm:text-sm border-gray-300 rounded-md"
+        :placeholder="placeholder"
+        :value="modelValue"
+        @input="$emit('update:modelValue', ($event.target as HTMLInputElement).value)"
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: 'Textarea',
+  props: {
+    name: String,
+    label: String,
+    placeholder: String,
+    modelValue: String,
+  }
+})
+</script>
