caa6089b67e4316ad654b4f30b0c3b91aedcb927
Marisa Robayo <marisarobayo@gmail.com>
2022-02-25 18:10:53 +0100

    Designs card group as radio buttons.

---
+++ src/components/HorizontalRadioGroup.vue
@@ -0,0 +1,37 @@
+
+<template>
+  <div class="flex justify-between items-center gap-x-8">
+    <div v-for="option in options" :key="option.value" class="flex-1">
+      <label>
+        <div
+          class="shadow-sm rounded-lg bg-white focus:outline-none flex flex-col gap-y-1 cursor-pointer p-4"
+          :class="option.value === modelValue ? 'border-2 border-teal-600' : 'border border-gray-300'"
+          @click="$emit('update:modelValue', option.value)"
+        >
+          <span class="font-medium text-lg">{{option.header}}</span>
+          <span class="text-zinc-600">{{option.subheader}}</span>
+        </div>
+      </label>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+
+interface Option {
+  value: string,
+  header: string,
+  subheader: string,
+}
+
+export default defineComponent({
+  name: 'HorizontalRadioGroup',
+  props: {
+    name: String,
+    modelValue: String,
+    options: Array as PropType<Array<Option>>
+  },
+  emits: ['update:modelValue'],
+})
+</script>
