62e57439b75a9bbfe95c4bf090f21c6da142bb85
Marisa Robayo <marisarobayo@gmail.com>
2022-02-28 12:59:38 +0100

    Changes color of select input.

--- src/components/Select.vue
+++ src/components/Select.vue
@@ -5,7 +5,7 @@
     <select
       :id="name"
       :name="name"
-      class="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+      class="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-teal-500 focus:border-teal-500 sm:text-sm rounded-md"
       :value="modelValue"
       :placeholder="placeholder"
       @change="$emit('update:modelValue', ($event.target as HTMLInputElement).value)"
