f6bc32070f9f4ab5dd0c2932bdeb6940355db635
Marisa Robayo <marisarobayo@gmail.com>
2022-03-11 19:40:39 +0100

    Allows disabling the primary button.

--- src/components/ButtonPrimary.vue
+++ src/components/ButtonPrimary.vue
@@ -2,16 +2,32 @@
 <template>
   <button
     type="button"
-    class="inline-flex items-center py-2 px-4 text-sm font-medium rounded-2xl shadow-sm cursor-pointer bg-teal-600 text-zinc-100 hover:bg-teal-700"
+    :class="styles"
+    class="inline-flex items-center py-2 px-4 text-sm font-medium rounded-2xl shadow-sm text-zinc-100 "
+    :disabled="disabled"
   >
     <slot></slot>
   </button>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { computed, defineComponent } from "vue";
 
 export default defineComponent({
   name: 'ButtonPrimary',
+  emits: ['onClick'],
+  props: {
+    disabled: Boolean,
+  },
+  setup: (props) => {
+    const styles = computed(() => ({
+      'bg-zinc-400': props.disabled,
+      'bg-teal-600 hover:bg-teal-700 cursor-pointer': !props.disabled,
+    }))
+
+    return {
+      styles,
+    }
+  }
 })
 </script>
