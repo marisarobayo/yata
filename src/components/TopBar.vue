9fe8da52c328a644d56b476bffa9ae2b0992948a
Marisa Robayo <marisarobayo@gmail.com>
2022-04-14 16:29:10 +0200

    Formats the top bar texts.

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -3,7 +3,7 @@
   <nav class="fixed top-0 left-0 w-full bg-white shadow-sm">
     <div class="flex justify-between w-full max-w-7xl py-1 mx-auto text-gravel-700">
       <div class="p-2">
-        <span class="text-3xl tracking-wider font-bold">YATA</span>
+        <span class="text-3xl tracking-wider font-bold cursor-default">YATA</span>
       </div>
       <div class="p-2" v-if="!user">
         <ButtonPrimary @click="$router.push('/signin')">Iniciar sesión</ButtonPrimary>
@@ -30,7 +30,7 @@
           <div class="absolute top-10 right-4 w-32 bg-zinc-50 shadow-xl py-3 rounded-xl border border-zinc-200" v-if="userMenuOpen">
             <div class="w-full">
               <button class="hover:bg-zinc-200 w-full py-1 rounded-sm" @click="logout">
-                Log out
+                Cerrar sesión
               </button>
             </div>
           </div>
