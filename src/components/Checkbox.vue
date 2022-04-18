2a46d5e2f2e77b695c963481efe55085fe04f13f
Marisa Robayo <marisarobayo@gmail.com>
2022-04-18 16:45:45 +0200

    Emits an event on change.

--- src/components/Checkbox.vue
+++ src/components/Checkbox.vue
@@ -5,6 +5,7 @@
       v-model="isChecked"
       type="checkbox"
       class="focus:ring-teal-500 h-4 w-4 text-teal-600 border-gray-300 rounded"
+      @change.stop="(event) => onChange(event)"
     />
   </span>
 </template>
@@ -14,14 +15,21 @@ import { defineComponent, ref } from "vue";
 
 export default defineComponent({
   name: 'Checkbox',
+  emits: ['change'],
   props: {
     checked: Boolean,
   },
-  setup: (props) => {
+  setup: (props, context) => {
     let isChecked = ref(props.checked)
+
+    const onChange = (event: Event) => {
+      const target = (event.target) as unknown as { checked: boolean }
+      context.emit('change', target.checked)
+    }
 
     return {
       isChecked,
+      onChange,
     }
   }
 })
